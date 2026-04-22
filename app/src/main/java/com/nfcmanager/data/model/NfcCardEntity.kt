8454c50312ba3de6a7bdd3f36de366de57d72ba6
kozvits <kozvits@gmail.com>
2026-04-22 12:23:32 +0300

    Add files via upload

--- app/src/main/java/com/nfcmanager/data/model/NfcCardEntity.kt
+++ app/src/main/java/com/nfcmanager/data/model/NfcCardEntity.kt
@@ -7,7 +7,8 @@ import androidx.room.PrimaryKey
 data class NfcCardEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
-    val uid: String,
+    val uid: String,          // MSB: BA85AC95
+    val uidLsb: String = "",  // LSB: 95AC85BA
     val name: String,
     val cardType: String,
     val techListJson: String,
